aca32e1af64cb99fd34e845cd402b997c9375e51
Kushan Zaveri <kushan.zaveri@gmail.com>
2020-04-29 11:39:17 -0400

    Cleaned up edge detector

--- EdgeDrawing.py
+++ EdgeDrawing.py
@@ -9,14 +9,14 @@ scan_interval = 3
 ksize_gaussian = 5
 sigma_gaussian = 1
 ksize_sobel = 3
-gradient_thresh = 400
-# f = open("out.txt", "x")
+gradient_thresh = 36
+
 class EdgeDrawing:
   def __init__(self):
     self.anchors = []
     self.G = []
     self.ED = []
-    self.included = []
+    self.visited = []
     self.edgels = []
     self.ROWS = -1
     self.COLS = -1
@@ -82,130 +82,72 @@ class EdgeDrawing:
   def __getU(self, row, col):
     return -1 if row + 1 == self.ROWS else self.__getUD(row + 1, col)
 
-  def __proceedLeft(self, row, col):
-#    f.write("LSTART %d %d\n"%(row, col))
+  def __proceedLR(self, row, col, row_fn, col_fn):
     while True:
-      self.included[row][col] = True
-      next_row = self.__getL(row, col)
-      if next_row == -1:
+      self.visited[row][col] = True
+      next_row = row_fn(row, col)
+      next_col = col_fn(col)
+      if next_row == -1 or next_col == -1:
         return
       row = next_row
-      col -= 1
-#      f.write("L --  %d %d\n"%(row, col))
-      if self.G[row][col] <= 0 or self.included[row][col]:
+      col = next_col
+      if self.G[row][col] <= 0 or self.visited[row][col]:
         return
       if self.ED[row][col] == VERTICAL:
-#        f.write("L // \n")
         break
     self.__proceed(row, col)
 
-  def __proceedRight(self, row, col):
-#    f.write("RSTART %d %d\n"%(row, col))
+  def __proceedUD(self, row, col, row_fn, col_fn):
     while True:
-      self.included[row][col] = True
-      next_row = self.__getR(row, col)
-      if next_row == -1:
+      self.visited[row][col] = True
+      next_col = col_fn(row, col)
+      next_row = row_fn(row)
+      if next_col == -1 or next_row == -1:
         return
+      col = next_col
       row = next_row
-      col += 1
-#      f.write("R --  %d %d\n"%(row, col))
-      if self.G[row][col] <= 0 or self.included[row][col]:
-        return
-      if self.ED[row][col] == VERTICAL:
-#        f.write("R // \n")
-        break
-    self.__proceed(row, col)
-
-  def __proceedUp(self, row, col):
-#    f.write("USTART %d %d\n"%(row, col))
-    while True:
-      self.included[row][col] = True
-      next_col = self.__getU(row, col)
-      if next_col == -1:
-        return
-      col = next_col
-      row += 1
-#      f.write("U --  %d %d\n"%(row, col))
-      if self.G[row][col] <= 0 or self.included[row][col]:
+      if self.G[row][col] <= 0 or self.visited[row][col]:
         return
       if self.ED[row][col] == HORIZONTAL:
-#        f.write("U // \n")
-        break
-    self.__proceed(row, col)
-
-  def __proceedDown(self, row, col):
-#    f.write("DSTART %d %d\n"%(row, col))
-    while True:
-      self.included[row][col] = True
-      next_col = self.__getD(row, col)
-      if next_col == -1:
-        return
-      col = next_col
-      row -= 1
-      #f.write("D --  %d %d\n"%(row, col))
-      if self.G[row][col] <= 0 or self.included[row][col]:
-        return
-      if self.ED[row][col] == HORIZONTAL:
-#        f.write("D // \n")
         break
     self.__proceed(row, col)
 
   def __proceed(self, row, col):
-    if self.included[row][col]:
+    if self.visited[row][col]:
       return
+    inc = lambda a: a + 1
+    dec = lambda a: a - 1
     if self.ED[row][col] == HORIZONTAL:
-      self.__proceedLeft(row, col)
-      self.__proceedRight(row, col)
+      self.__proceedLR(row, col, self.__getL, dec)
+      self.__proceedLR(row, col, self.__getR, inc)
 
     if self.ED[row][col] == VERTICAL:
-      self.__proceedDown(row, col)
-      self.__proceedUp(row, col)
+      self.__proceedUD(row, col, dec, self.__getD)
+      self.__proceedUD(row, col, inc, self.__getU)
 
   def ConnectAnchors(self):
-    self.included = np.zeros(np.shape(self.G), dtype = bool)
+    self.visited = np.zeros(np.shape(self.G), dtype = bool)
     for [row, col] in self.anchors:
-      if not self.included[row][col]:
+      if not self.visited[row][col]:
         self.__proceed(row, col)
 
     for row in range(self.ROWS):
       for col in range(self.COLS):
-        if self.included[row][col]:
+        if self.visited[row][col]:
           e.edgels.append([row, col])
     
 if __name__=="__main__":
   e = EdgeDrawing()
-#  img = e.GaussianFilter("DSC_0426.JPG")
-  img = e.GaussianFilter("Mickey_Mouse.jpg")
-  #img = e.GaussianFilter("lenna.png")
+  img = e.GaussianFilter("sample.png")
   e.SobelOperator(img) 
   e.FindAnchors()
   e.ConnectAnchors()
+  
   x, y = [], []
   for [x_, y_] in e.edgels:
     x.append(y_)
     y.append(x_)
-  
-  x_2, x_3 = [], []
-  y_2, y_3 = [], []
-  for r in range(e.ROWS):
-    for c in range(e.COLS):
-      if e.ED[r][c] == HORIZONTAL:
-        x_2.append(c)
-        y_2.append(r)
-      elif e.ED[r][c] == VERTICAL:
-        x_3.append(c)
-        y_3.append(r)
 
- # plt.figure(figsize=(10, 10))
-#  f.close()
- # plt.scatter(x_2, y_2, marker = '3', color='g')
- # plt.scatter(x_3, y_3, marker = '2', color='b')
-#  plt.scatter(x, y, marker = ',', color='y')
-#  plt.gca().set_aspect('equal', adjustable='box')
-#  ax = plt.gca()
-#  ax.invert_yaxis()
- # plt.imshow(e.G, cmap='gray')
-  fig = plt.figure()
-  ax = fig.add_subplot(111)
-  p = ax.plot(x[:100], y[:100], 'b')
-  ax.invert_yaxis()
+  plt.scatter(x, y, marker=',', color='r')
+  plt.imshow(e.G, cmap='gray')
+  plt.show()
